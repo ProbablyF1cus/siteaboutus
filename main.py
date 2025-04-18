57936c9536185ee1a25ea01a064667e1712cd384
KachokChort <timajambam@gmail.com>
2025-04-18 20:56:56 +0300

    First

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from flask import request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def cookie_test():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
