798aca84a6af8adc97ed2255399fa5822e4a9f9a
KachokChort <timajambam@gmail.com>
2025-04-27 17:45:57 +0300

    Sing up and sing in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
-from flask import request, make_response
+from flask import Flask, render_template, request, make_response
+from data.users import User
+from data import db_session
 
 app = Flask(__name__)
 
@@ -8,9 +8,52 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login")
-def cookie_test():
-    return render_template('login.html')
+@app.route("/sing_up")
+def sing_up():
+    return render_template('sing_up.html')
+
+
+@app.route("/sing_in")
+def sing_in():
+    return render_template('sing_in.html')
+
+
+@app.route("/submit_sing_up", methods=['POST', 'GET'])
+def submit_sing_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    password2 = request.form.get("confirm_password")
+    if "@" not in email or len(email) < 5:
+        return render_template("error.html", error="Плохой Email")
+    if len(username) < 3 or '/' in username:
+        return render_template("error.html", error="Короткий username")
+    if len(password) < 3:
+        return render_template("error.html", error="Короткий password")
+    if password != password2:
+        return render_template("error.html", error="Пароли не совпадают")
+
+
+    user = User()
+    user.name = username
+    user.email = email
+    user.hashed_password = password
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    return f"Спасибо, за регистрацию {username}!"
+
+
+@app.route("/submit_sing_in", methods=['POST', 'GET'])
+def submit_sing_in():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    return f"Спасибо, за регистрацию {username}!"
+
+
 
 if __name__ == '__main__':
+    db_session.global_init('db/db.db')
     app.run(port=8080, host='127.0.0.1')
---
+++ test_db.py
@@ -0,0 +1,9 @@
+from data.users import User
+from data import db_session
+
+db_session.global_init('db/db.db')
+
+
+db_sess = db_session.create_session()
+for user in db_sess.query(User).all():
+    print(user.name)
